dc2cac6b55e5da969c8b895b77ec666f7f623fa8
pokm321 <pokm90@gmail.com>
2022-05-12 10:43:49 +0900

    Practice with javascript (alert, confirm, prompt, var, let, etc.)

---
+++ day4/my_scripts.js
@@ -0,0 +1,12 @@
+let i = 10;
+let j = 10;
+var name = '안계현';
+
+const STUDENT_COUNT = 10;
+
+var result = name + '은' + i + '살입니다.';
+result = `${name}은${i}살입니다`;
+
+function showMyName() {
+    alert('안계현');
+}
